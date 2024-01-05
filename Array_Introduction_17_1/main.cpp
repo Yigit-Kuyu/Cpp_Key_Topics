92126e16d17a6662184e0b2e1a8398f4c34b69d0
Yigit-Kuyu <yigitkuyu@gmail.com>
2024-01-05 23:42:02 +0300

    V1

--- Array_Introduction_17_1/main.cpp
+++ Array_Introduction_17_1/main.cpp
@@ -55,7 +55,7 @@ int main()
     // ############# Accessing array elements using operator[]:
     constexpr std::array<int, 5> prime{ 2, 3, 5, 7, 11 };
     std::cout << prime[3]; // print the value of element with index 3 (7)
-    std::cout << prime[9]; // invalid index (undefined behavior)
+    // std::cout << prime[9]; // invalid index (undefined behavior)
 
 
 
--- Array_Returning_17_3/main.cpp
+++ Array_Returning_17_3/main.cpp
@@ -1,11 +1,55 @@
+#include <array>
 #include <iostream>
 
-using namespace std;
+/*
+Syntax aside, passing a std::array to a function is conceptually simple -- pass it by (const) reference.
+But what if we have a function that needs to return a std::array? Things are a little more complicated.
+Unlike std::vector, std::array is not move-capable, so returning a std::array by value will make a copy of the array.
+For move semantics property in vectors: see Vector_Returning_16_4
+
+So, there are three options to return:
+      - return by value: returning directly std::array if it is not expensive
+      - return via out parameter: passing in the std::array by non-const reference (or by address), and the function can then modify this array.
+      - return std::vector instead: see Vector_Returning_16_4
+
+*/
+
+
+// return by value:
+template <typename T, std::size_t N>
+std::array<T, N> inputArray_byValue(std::array<T, N>& arr)
+{
+
+	arr[2]=1;
+
+
+	return arr;
+}
+
+
+// return via out parameter:
+template <typename T, std::size_t N>
+void inputArray_byParameter(std::array<T, N>& arr2)
+{
+
+	arr2[2]=5;
+
+}
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+	std::array<int, 5>yck{};
+	std::array<int, 5> arr=inputArray_byValue(yck);
+
+	std::cout << " return-by-value of element 2 is " << arr[2] << '\n';
+
+	inputArray_byParameter(yck);
+
+	std::cout << " return-via-out-parameter of element 2 is " << yck[2] << '\n';
+
+	return 0;
 }
 
 
